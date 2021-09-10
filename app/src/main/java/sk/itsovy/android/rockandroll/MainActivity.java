3df1aae8a6b0b8283206d7f0a9a66dd9125cc002
miroslav-opiela <miroslav-opiela@upjs.sk>
2021-09-10 12:28:16 +0200

    funguje roll

--- app/src/main/java/sk/itsovy/android/rockandroll/MainActivity.java
+++ app/src/main/java/sk/itsovy/android/rockandroll/MainActivity.java
@@ -3,12 +3,27 @@ package sk.itsovy.android.rockandroll;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonRoll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonRoll = findViewById(R.id.button_roll);
+    }
+
+    public void roll(View view) {
+        Random random = new Random();
+        int number = random.nextInt(6) + 1;
+        // musime dat string, int by hladal medzi resources v triede R
+        buttonRoll.setText(String.valueOf(number));
     }
 }
